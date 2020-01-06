da3e198030c210435b06fd7c0bb2064fd04a3317
carson <782610180@qq.com>
2020-01-06 20:20:51 +0800

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      env
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Board/>
+            </div>
+        )
+    }
 }
 
 export default App;
+
+/**
+ *
+ */
+class Board extends React.Component {
+    render() {
+        return (
+            <div className="row-wrapper">
+                <div className="row">
+                    <div>1</div>
+                    <div>1</div>
+                    <div>1</div>
+                </div>
+                <div className="row">
+                    <div>1</div>
+                    <div>1</div>
+                    <div>1</div>
+                </div>
+                <div className="row">
+                    <div>1</div>
+                    <div>1</div>
+                    <div>1</div>
+                </div>
+            </div>
+        )
+    }
+}
