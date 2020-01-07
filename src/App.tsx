67b38e7ddd64023c24c86eb523d30e2688b8ffe4
carson <782610180@qq.com>
2020-01-07 21:57:23 +0800

    add event listener

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,39 @@ interface IAppProps {
 }
 
 interface IAppState {
-
+    board: string[];
+    curSymbol: string;
 }
 
 class App extends React.Component<IAppProps, IAppState> {
-    constructor(props:any) {
+    constructor(props: any) {
         super(props);
-        this.state = {}
+        this.state = {
+            board: new Array(9).fill('O'),
+            curSymbol: 'x',
+        }
     }
 
-    handleClick = () => {
-        console.log('clicked ');
+    handleClick = (index: number) => {
+        const updatedSymbol = this.flipCurrentSymbol(this.state.curSymbol);
+        this.setState({
+            curSymbol: updatedSymbol
+        });
+
+        this.state.board[index] = this.state.curSymbol;
+        this.setState({
+            board:this.state.board
+        })
+    };
+
+    flipCurrentSymbol = (symbol: string): string => {
+        return symbol === 'X' ? 'O' : 'X';
     };
 
     render() {
         return (
             <div className="App">
-                <Board onClick={this.handleClick}/>
+                <Board onClick={this.handleClick} array={this.state.board}/>
             </div>
         )
     }
@@ -35,7 +51,8 @@ export default App;
  *
  */
 interface IBoardProps {
-    onClick: () => void;
+    array: string[];
+    onClick: (index: number) => void;
 }
 
 interface IBoardState {
@@ -47,19 +64,19 @@ class Board extends React.Component<IBoardProps, IBoardState> {
         return (
             <div className="row-wrapper">
                 <div className="row">
-                    <div onClick={this.props.onClick}>1</div>
-                    <div onClick={this.props.onClick}>1</div>
-                    <div onClick={this.props.onClick}>1</div>
+                    <div onClick={(e: any) => this.props.onClick(e)}>{this.props.array[0]}</div>
+                    <div onClick={(e: any) => this.props.onClick(e)}>{this.props.array[1]}</div>
+                    <div onClick={(e: any) => this.props.onClick(e)}>{this.props.array[2]}</div>
                 </div>
                 <div className="row">
-                    <div onClick={this.props.onClick}>1</div>
-                    <div onClick={this.props.onClick}>1</div>
-                    <div onClick={this.props.onClick}>1</div>
+                    <div onClick={(e: any) => this.props.onClick(e)}>{this.props.array[3]}</div>
+                    <div onClick={(e: any) => this.props.onClick(e)}>{this.props.array[4]}</div>
+                    <div onClick={(e: any) => this.props.onClick(e)}>{this.props.array[5]}</div>
                 </div>
                 <div className="row">
-                    <div onClick={this.props.onClick}>1</div>
-                    <div onClick={this.props.onClick}>1</div>
-                    <div onClick={this.props.onClick}>1</div>
+                    <div onClick={(e: any) => this.props.onClick(e)}>{this.props.array[6]}</div>
+                    <div onClick={(e: any) => this.props.onClick(e)}>{this.props.array[7]}</div>
+                    <div onClick={(e: any) => this.props.onClick(e)}>{this.props.array[8]}</div>
                 </div>
             </div>
         )
