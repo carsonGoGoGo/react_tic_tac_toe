780aab80fe37071717579b3e8532a60e467209ab
carson <782610180@qq.com>
2020-01-22 18:33:00 +0800

    add mongoose for user login

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import {Schema} from 'mongoose'
+import * as mongoose from "mongoose";
+
+export interface IUser {
+    userName: string;
+    email: string;
+    password: string;
+    city: string;
+}
+
+export var userSchema = new Schema({
+    userName: String,
+    email: String,
+    password: String,
+    city: String
+});
+
+const db = mongoose.connection;
+db.on('error', function () {
+    
+});
