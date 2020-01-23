0a9d3d381ea18e8689bcefd0900cec0a5bb5dff3
carson <782610180@qq.com>
2020-01-23 17:20:52 +0800

    add compile on change to ide

--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,41 @@
+import * as mongoose from 'mongoose'
 import {Schema} from 'mongoose'
-import * as mongoose from "mongoose";
-
-export interface IUser {
-    userName: string;
-    email: string;
-    password: string;
-    city: string;
-}
-
-
-const db = mongoose.connection;
 
 const db_url = 'mongodb://localhost/myTest';
 
 /**
  * 连接到数据库
  */
-mongoose.connect(db_url).then(()=>{
+mongoose.connect(db_url);
+
+mongoose.connection.on('connected', function () {
+
     console.log("connect to db successfully");
 
-    var userSchema = new Schema({
-        userName: String,
-        email: String,
-        password: String,
-        city: String
-    });
+    function saveUser() {
+        var userSchema = new Schema({
+            user_name: String,
+            email: String,
+            password: String,
+            city: String
+        });
 
-    var userModel = mongoose.model('UserName', userSchema);
+        var userModel = mongoose.model('UserName', userSchema);
+        var myFirstUser = new userModel({
+            user_name: 'putilaozu',
+            email: 'putilaozu@qq.com',
+            password: '12345',
+            city: 'heaven'
+        });
+        myFirstUser.save(function (error, rst) {
+            if (error) {
+                console.log('save error 00');
+            } else {
+                return "rst" + rst;
+            }
+        })
+    }
 
 
-    var myFirstUser = new userModel({
-        userName: 'putilaozu',
-        email: 'putilaozu@qq.com',
-        password: '12345',
-        city: 'heaven'
-    });
-
-    myFirstUser.save(function(error){
-        console.log('save error');
-    })
-
-
-
+    saveUser();
 });
